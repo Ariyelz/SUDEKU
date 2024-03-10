414d1693fe13ef27c602d05c569dc8a306c15d92
Ariyelz <line_bratli@hotmail.com>
2024-03-10 14:45:54 +0100

    Getting dizzy with logic of rows n columns, but think i have it right.

--- SushiBoard.cs
+++ SushiBoard.cs
@@ -38,4 +38,61 @@ public class SushiBoard
 
         return true; //If sushiboard is valid.
     }
+
+    private bool IsValidRow(int row)
+    {
+        HashSet<char> set = new HashSet<char>(); //Using hashset to track unique characters in row.
+        for (int col = 0; col < _columns; col++)
+        {
+            char block = _board[row][col]; // Gets char in block for x row,col
+            if (block != '-') // If block is not '-'
+            {
+                if (set.Contains(block))
+                    return false; //  Duplicate in row
+                set.Add(block); // adding block to Hashset
+            }
+        }
+
+        return true; //row is valid
+    }
+
+    private bool IsValidColumn(int column)
+    {
+        HashSet<char> set = new HashSet<char>();
+        for (int row = 0; row < _rows; row++)
+        {
+            char block = _board[row][column];
+            if (block != '-')
+            {
+                if (set.Contains(block))
+                    return false; //Duplicate in column
+                set.Add(block);
+            }
+        }
+
+        return true;
+    }
+
+    private bool IsValidBox(int box)
+    {
+        HashSet<char> set = new HashSet<char>();
+        int startRow = (box / 3) * 3; //Startrow in box
+        int startColumn = (box % 3) * 3; // Startcolumn in box
+
+        for (int row = 0; row < startRow + 3; row++)
+        {
+            for (int col = startColumn; col < startColumn + 3; col++)
+            {
+                char block = _board[row][col];
+                if (block != '-')
+                {
+                    if (set.Contains(block))
+                        return false; //Duplicate in 3x3 box
+                    set.Add(block);
+                }
+            }
+        }
+
+        return true;
+    }
 }
